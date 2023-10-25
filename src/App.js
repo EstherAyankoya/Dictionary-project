214426f1b0adb72af4284d782bcdba227b2a0843
EstherAyankoya <estherayankoya@gmail.com>
2023-10-25 21:17:46 +0100

    dictionary compnonent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-     hi
+      <div className="container">
+        hi
+        <main><Dictionary /></main>
+        <footer>coded by esther</footer>
+    </div>
     </div>
   );
 }
